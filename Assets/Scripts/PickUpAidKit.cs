92ae1d849948cad4a5a8e09267076415059a0286
Ahmad Abdu <l000000k32@yahoo.com>
2022-12-07 15:59:51 +0200

    some changes and effects

---
+++ Assets/Scripts/PickUpAidKit.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpAidKit : MonoBehaviour
+{
+    public GameObject TheAidKit;
+    public GameObject AidKitBoxLight;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (GlobalHealth.currentHealth < 100)
+        {
+            GlobalHealth.currentHealth += 25;
+            TheAidKit.SetActive(false);
+            AidKitBoxLight.SetActive(false);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Image soundOn;
+    [SerializeField] Image soundOff;
+    private bool muted = false;
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("muted"))
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+        changeIcon();
+        AudioListener.pause = muted;
+    }
+    public void onButtonPressed()
+    {
+        if (muted == false)
+        {
+            muted = true;
+            AudioListener.pause = true;
+        }
+        else
+        {
+            muted = false;
+            AudioListener.pause = false;
+        }
+        save();
+        changeIcon();
+    }
+    private void changeIcon()
+    {
+        if (muted == false)
+        {
+            soundOn.enabled = true;
+            soundOff.enabled = false;
+        }
+        else
+        {
+            soundOn.enabled = false;
+            soundOff.enabled = true;
+        }
+    }
+
+    private void Load()
+    {
+        muted = PlayerPrefs.GetInt("muted") == 1;
+    }
+    private void save()
+    {
+        PlayerPrefs.SetInt("muted", muted ? 1 : 0);
+    }
+
+}
